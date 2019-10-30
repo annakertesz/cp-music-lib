4105fbc5a928bccc5de331f4470ff6cdaa31f98e
Anna-Kertesz <anna.kertesz@ibm.com>
2019-10-30 14:05:40 +0100

    working server

---
+++ src/models/user.go
@@ -0,0 +1,13 @@
+/* © Copyright IBM Corp. 2018
+All Rights Reserved.
+This software is the confidential and proprietary information
+of the IBM Corporation. (‘Confidential Information’). Redistribution
+of the source code or binary form is not permitted without prior authorization
+from the IBM Corporation.
+*/
+
+package models
+
+type user struct {
+
+}
