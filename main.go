0513d614af4526aa420f02b3d96ba3db59a2107e
Anna Kertesz <anna.kertesz@ibm.com>
2019-12-16 14:37:23 +0100

    bla

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
+
 import (
 	"fmt"
-	"github.com/annakertesz/cp-music-lib/library"
 	"log"
 	"net/http"
 	"os"
@@ -14,7 +14,7 @@ func determineListenAddress() (string, error) {
 	return ":" + port, nil
 }
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, library.Function(4,7))
+	fmt.Fprintln(w, "library.Function(4,7)")
 }
 func main() {
 	addr, err := determineListenAddress()
