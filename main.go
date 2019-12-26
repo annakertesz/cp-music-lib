f607ea1f4696b0e1fddd70af5d8dcb68edab1179
Anna Kertesz <anna.kertesz@ibm.com>
2019-12-26 08:41:39 +0100

    add chi router

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/annakertesz/cp-music-lib/library"
+	"github.com/go-chi/chi"
 	"log"
 	"net/http"
 	"os"
 )
+
 func determineListenAddress() (string, error) {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -14,18 +16,35 @@ func determineListenAddress() (string, error) {
 	}
 	return ":" + port, nil
 }
+
 func hello(w http.ResponseWriter, r *http.Request) {
-	x := library.Function(5,6)
+	x := library.Function(5, 6)
 	fmt.Fprintf(w, "library.Function(4,7) %v", x)
 }
+
 func main() {
 	addr, err := determineListenAddress()
 	if err != nil {
 		log.Fatal(err)
 	}
-	http.HandleFunc("/", hello)
-	log.Printf("Listening on %s...\n", addr)
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		panic(err)
+	//http.HandleFunc("/", hello)
+	//log.Printf("Listening on %s...\n", addr)
+	//if err := http.ListenAndServe(addr, nil); err != nil {
+	//	panic(err)
+	//}
+
+	log.Println("Started")
+	if err := http.ListenAndServe(addr, Routes()); err != nil {
+		log.Fatal("Could not start HTTP server", err.Error())
 	}
+}
+
+func Routes() chi.Router {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintln(w, "cp-audio-lib")
+	})
+
+	return r
 }
