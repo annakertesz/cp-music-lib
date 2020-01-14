46cc366665770622cd7db4b70cf4ed6cd80de9b7
Anna Kertesz <anna.kertesz@ibm.com>
2020-01-14 19:17:10 +0100

    add transport layer

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/annakertesz/cp-music-lib/library"
-	"github.com/go-chi/chi"
+	"github.com/annakertesz/cp-music-lib/transport"
 	"log"
 	"net/http"
 	"os"
@@ -17,34 +16,15 @@ func determineListenAddress() (string, error) {
 	return ":" + port, nil
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	x := library.Function(5, 6)
-	fmt.Fprintf(w, "library.Function(4,7) %v", x)
-}
-
 func main() {
 	addr, err := determineListenAddress()
 	if err != nil {
 		log.Fatal(err)
 	}
-	//http.HandleFunc("/", hello)
-	//log.Printf("Listening on %s...\n", addr)
-	//if err := http.ListenAndServe(addr, nil); err != nil {
-	//	panic(err)
-	//}
-
 	log.Println("Started")
-	if err := http.ListenAndServe(addr, Routes()); err != nil {
+	if err := http.ListenAndServe(addr, transport.Routes()); err != nil {
 		log.Fatal("Could not start HTTP server", err.Error())
 	}
 }
 
-func Routes() chi.Router {
-	r := chi.NewRouter()
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintln(w, "cp-audio-lib")
-	})
-
-	return r
-}
