9b95eba8494b05c0b7559bc6f74c61176a7b9e36
anna <kerteszannanak@gmail.com>
2020-06-01 23:35:37 +0200

    add more headers for cors

--- controller/router.go
+++ controller/router.go
@@ -127,8 +127,8 @@ func (server *Server) Routes() chi.Router {
 	})
 	r.Options("/user", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token, Accept, Content-Length")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token, Accept, Content-Length, Accept-Encoding, X-CSRF-TOKEN, Authorization")
 		w.Header().Set("Access-Control-Max-Age", "86400")
 		w.WriteHeader(http.StatusOK)
 	})
