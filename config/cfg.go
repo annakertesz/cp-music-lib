21e9a381715bc57e3234c9964e276c34f7f068d6
Matyas Forian Szabo <forianszm@gmail.com>
2021-01-03 21:24:21 +0100

    add SMTP sender

--- config/cfg.go
+++ config/cfg.go
@@ -1,27 +1,33 @@
 package config
 
-
 type Config struct {
-	BoxConfig BoxConfig
-	SengridConfig SengridConfig
-	SongFolder int
-	CoverFolder int
+	BoxConfig      BoxConfig
+	SengridConfig  SengridConfig
+	SongFolder     int
+	CoverFolder    int
 	DefaultPicture int
-	PsqlInfo string
-	Url string
+	PsqlInfo       string
+	Url            string
 }
 
 type SengridConfig struct {
-	SengridAPIKey string
-	SenderName string
-	SenderEmail string
-	AdminEmail string
+	SengridAPIKey  string
+	SenderName     string
+	SenderEmail    string
+	AdminEmail     string
 	DeveloperEmail string
 }
 
 type BoxConfig struct {
-	ClientID string
+	ClientID     string
 	ClientSecret string
-	PrivateKey string
-	Token string
+	PrivateKey   string
+	Token        string
+}
+
+type SmtpEmailConfig struct {
+	ServerAddress string //e.g. smtp.gmail.com:587
+	UserName      string
+	Password      string
+	Host          string // e.g. smtp.gmail.com
 }
